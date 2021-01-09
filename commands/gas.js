234bbdf7c382341edfd1d017e6cd0c544ded7f1a
Antonio Savage <asavage@bisontrails.co>
2021-01-09 12:56:13 -0600

    displaying gas price in readable units

--- commands/gas.js
+++ commands/gas.js
@@ -2,18 +2,20 @@
 
 const axios = require('axios').default;
 const url = 'https://www.gasnow.org/api/v3/gas/price';
+const web3utils = require('web3-utils');
 
 module.exports = {
-    name: 'gas',
-    description: 'get eth gas price',
-    async execute(message) {
-      axios.get(url).then(function (response) {
-        const gasPrice = response.data.data;
-        message.channel.send(`
-rapid: ${gasPrice.rapid}
-fast: ${gasPrice.fast}
-standard: ${gasPrice.standard}
-slow: ${gasPrice.slow}`);
-      });
-    },
-  };
+  name: 'gas',
+  description: 'get eth gas price',
+  async execute(message) {
+    axios.get(url).then(function (response) {
+      const gasPrice = response.data.data;
+      let BN = web3utils.BN;
+      message.channel.send(`
+rapid: ${web3utils.fromWei(new BN(gasPrice.rapid).toString(), 'gwei')}
+fast: ${web3utils.fromWei(new BN(gasPrice.fast).toString(), 'gwei')}
+standard: ${web3utils.fromWei(new BN(gasPrice.standard).toString(), 'gwei')}
+slow: ${web3utils.fromWei(new BN(gasPrice.slow).toString(), 'gwei')}`);
+    });
+  },
+};
