7b166c3d65bd3972cf86baf610b81930236a136f
Antonio Savage <afmsavage@gmail.com>
2021-12-26 21:53:18 -0500

    whoops

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 const blocknative = 'https://api.blocknative.com/gasprices/blockprices';
 // const web3utils = require('web3-utils');
 
-const blocknativeAPI = '712b5e7c-1bf2-44b8-8fb6-d41cd4ce1e8a';
+const blocknativeAPI = process.env.BLOCKNATIVE;
 
 const main = async () => {
   axios
